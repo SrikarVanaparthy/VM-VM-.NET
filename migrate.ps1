33c2f2dc11caf6de2ff1025e3508f52ba16ef414
Srikar Vanaparthy <svanaparthy@anergroup.com>
2025-05-20 20:57:53 +0530

    Added files

--- migrate.ps1
+++ migrate.ps1
@@ -6,11 +6,6 @@ param (
     [string]$DestinationPath
 )
 
-# Set secure permissions on the SSH private key file (Windows-safe)
-Write-Host "Setting secure permissions on private key file: $PrivateKeyPath"
-icacls $PrivateKeyPath /inheritance:r | Out-Null
-icacls $PrivateKeyPath /grant:r "$($env:USERNAME):(R)" | Out-Null
-icacls $PrivateKeyPath /remove "Users" | Out-Null
 
 # Construct full remote SCP path
 $remoteTarget = "${RemoteUser}@${RemoteIP}:`"$DestinationPath`""
